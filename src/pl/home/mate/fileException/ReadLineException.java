4c54d6d3d2271493264af755d2b1ba9ea820a392
Andrzejf <afornalik2@gmail.com>
2019-02-19 23:11:34 +0100

    small modification of Exception Class

--- src/pl/home/mate/fileException/ReadLineException.java
+++ src/pl/home/mate/fileException/ReadLineException.java
@@ -3,4 +3,10 @@ package pl.home.mate.fileException;
 
 public class ReadLineException extends Exception {
 
+    public ReadLineException() {
+    }
+
+    public ReadLineException(String message) {
+        super(message);
+    }
 }
--- src/pl/home/mate/textException/SplitTextException.java
+++ src/pl/home/mate/textException/SplitTextException.java
@@ -1,4 +1,10 @@
 package pl.home.mate.textException;
 
 public class SplitTextException extends Exception {
+    public SplitTextException() {
+    }
+
+    public SplitTextException(String message) {
+        super(message);
+    }
 }
